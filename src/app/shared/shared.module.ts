5621ad3b981ff2209af67bc1455db0e8b04128cd
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2024-03-17 11:24:00 +0100

    Add multiple headers according to screen definition

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { DesktopHeaderComponent } from './components/desktop-header/desktop-header.component';
+import { MobileHeaderComponent } from './components/mobile-header/mobile-header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DesktopHeaderComponent,
+    MobileHeaderComponent,
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    DesktopHeaderComponent,
+    MobileHeaderComponent
   ]
 })
 export class SharedModule { }
