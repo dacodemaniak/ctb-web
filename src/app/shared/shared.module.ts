8f0312db16613bbf4886914f94db6a2123b23ff9
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2024-03-18 16:28:28 +0100

    Add footers to layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DesktopHeaderComponent } from './components/desktop-header/desktop-header.component';
 import { MobileHeaderComponent } from './components/mobile-header/mobile-header.component';
+import { DesktopFooterComponent } from './components/desktop-footer/desktop-footer.component';
+import { MobileFooterComponent } from './components/mobile-footer/mobile-footer.component';
 
 
 @NgModule({
   declarations: [
     DesktopHeaderComponent,
     MobileHeaderComponent,
+    DesktopFooterComponent,
+    MobileFooterComponent,
   ],
   imports: [
     CommonModule
   ],
   exports: [
     DesktopHeaderComponent,
-    MobileHeaderComponent
+    MobileHeaderComponent,
+    DesktopFooterComponent,
+    MobileFooterComponent
   ]
 })
 export class SharedModule { }
